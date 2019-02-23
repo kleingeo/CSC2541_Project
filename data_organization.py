b71c175b9fa599fc39d1bd59a28dba52b500503f
Unknown <geoff.a.klein@gmail.com>
2019-02-23 12:44:45 -0500

    Updated image and slice organization; Updated gitignore to not include data
    Updated slicing and organizing script to slice each volume, save them and put them into specified save path. Just change directory path and run code to save slices. However, use the .csv and .pickle files in the github so there is consistent train, val and testing.

--- data_organization.py
+++ data_organization.py
@@ -1,152 +1,156 @@
 import pandas as pd
 import numpy as np
 import os
+import SimpleITK as sitk
 import nibabel as nib
 from nibabel.processing import resample_to_output
-from joblib import Parallel, delayed
-
-def build_df(df, file, idx, file_path_main_images, file_path_main_labels):
-    if file.startswith('prostate'):
-
-        file_split = file.split('.')[0].split('_')
-
-        img = nib.load(file_path_main_images + '/' + file)
-
-        img_size = img.shape
-
-        img_pix_size = img.header['pixdim'][1:4]
-
-        if os.path.exists(file_path_main_labels + '/' + file) is False:
-            OSError('Label missing for: ', file)
-
-        # pixdim_x.append(img_pix_size[0])
-        # pixdim_y.append(img_pix_size[1])
-        # pixdim_z.append(img_pix_size[2])
-
-        img_resampled = resample_to_output(img, voxel_sizes=[0.802734, 0.802734, 2.5])
-
-        img_resampeld_size = img_resampled.shape
-
-        img_resampeld_pix_size = img_resampled.header['pixdim'][1:4]
-
-        dict_hold = {'patient_number': file_split[-1],
-                     'pixdim_x': img_pix_size[0],
-                     'pixdim_y': img_pix_size[1],
-                     'pixdim_z': img_pix_size[2],
-
-                     'resampled_pixdim_x': img_resampeld_pix_size[0],
-                     'resampled_pixdim_y': img_resampeld_pix_size[1],
-                     'resampled_pixdim_z': img_resampeld_pix_size[2],
-
-                     'size_x': img_size[0],
-                     'size_y': img_size[1],
-                     'size_z': img_size[2],
-
-                     'resampled_size_x': img_resampeld_size[0],
-                     'resampled_size_y': img_resampeld_size[1],
-                     'resampled_size_z': img_resampeld_size[2],
-
-                     'filename': file,
-                     'image_path': file_path_main_images + '/' + file,
-                     'labe_path': file_path_main_labels + '/' + file}
-
-        df = df.append(dict_hold, ignore_index=True)
-        print()
-        print(idx)
-
-    return df
 
 
 if __name__ == '__main__':
 
-    file_path_main_images = 'D:/Geoff_Klein/Prostate_Data/Task05_Prostate/imagesTr'
-    file_path_main_labels = 'D:/Geoff_Klein/Prostate_Data/Task05_Prostate/labelsTr'
+    # Initialize seed for randomization
+    np.random.seed(1)
 
-    df = pd.DataFrame(columns=['patient_number', 'pixdim_x', 'pixdim_y', 'pixdim_z',
+    file_path_main_images = 'D:/Prostate_Data/Task05_Prostate/imagesTr'
+    file_path_main_labels = 'D:/Prostate_Data/Task05_Prostate/labelsTr'
+
+    total_slices = 602
+
+    N_train = int(total_slices * 0.7)
+    N_val = int(total_slices * 0.15)
+    N_test = int(total_slices * 0.15)
+
+    N_total = [N_train, N_val, N_test]
+
+    num_train = 0
+    num_val = 0
+    num_test = 0
+
+
+
+    df_vol = pd.DataFrame(columns=['patient_number', 'pixdim_x', 'pixdim_y', 'pixdim_z',
                                'resampled_pixdim_x', 'resampled_pixdim_y', 'resampled_pixdim_z',
                                'size_x', 'size_y', 'size_z',
                                'resampled_size_x', 'resampled_size_y', 'resampled_size_z',
-                               'filename', 'image_path', 'label_path'])
+                               'filename', 'image_path', 'label_path', 'train_val_test'])
 
-    pixdim_x = []
-    pixdim_y = []
-    pixdim_z = []
+    df_slice_sample = pd.DataFrame(columns=['patient_number', 'slice_number',
+                                            'resampled_pixdim_x', 'resampled_pixdim_y',
+                                            'resampled_size_x', 'resampled_size_y',
+                                            'image_filename', 'label_filename', 'train_val_test'])
 
 
-    resampled_x_size = []
-    resampled_y_size = []
-    resampled_z_size = []
+    file_list = os.listdir(file_path_main_images)
 
-    for idx, file in enumerate(os.listdir(file_path_main_images)):
+    np.random.shuffle(file_list)
+
+    for file in file_list:
 
         if file.startswith('prostate'):
 
             file_split = file.split('.')[0].split('_')
 
-
             img = nib.load(file_path_main_images + '/' + file)
+
+            if os.path.exists(file_path_main_labels + '/' + file) is False:
+                OSError('Label missing for: ', file)
+
+            label = nib.load(file_path_main_labels + '/' + file)
 
             img, img_ADC = nib.funcs.four_to_three(img)
 
             img_size = img.shape
 
+            N_sample_slices = img_size[2]
+
+            if (N_sample_slices + num_train) < N_train:
+                train_val_test = 'train'
+                num_train = num_train + N_sample_slices
+
+            elif (N_sample_slices + num_val) < N_val:
+                train_val_test = 'val'
+                num_val = num_val + N_sample_slices
+
+            else:
+                train_val_test = 'test'
+                num_test = num_test + N_sample_slices
+
             img_pix_size = img.header['pixdim'][1:4]
 
-            if os.path.exists(file_path_main_labels + '/' + file) is False:
-
-                OSError('Label missing for: ', file)
 
 
-            pixdim_x.append(img_pix_size[0])
-            pixdim_y.append(img_pix_size[1])
-            pixdim_z.append(img_pix_size[2])
+            # No need to resample slice axis only using 2D slices
+            img_resampled = resample_to_output(img, voxel_sizes=[0.625, 0.625, img_pix_size[2]])
 
-
-
-            # img_resampled = resample_to_output(img, voxel_sizes=[0.802734, 0.802734, 2.5])
-
-            img_resampled = resample_to_output(img, voxel_sizes=[0.6, 0.6, 3.6])
+            label_resampled = resample_to_output(img, voxel_sizes=[0.625, 0.625, img_pix_size[2]], order=0)
 
             img_resampeld_size = img_resampled.shape
-
             img_resampeld_pix_size = img_resampled.header['pixdim'][1:4]
 
-            resampled_x_size.append(img_resampeld_size[0])
-            resampled_y_size.append(img_resampeld_size[1])
-            resampled_z_size.append(img_resampeld_size[2])
 
-            dict_hold = {'patient_number': file_split[-1],
-                         'pixdim_x': img_pix_size[0],
-                         'pixdim_y': img_pix_size[1],
-                         'pixdim_z': img_pix_size[2],
+            dict_vol = {'patient_number': file_split[-1],
+                        'pixdim_x': img_pix_size[0],
+                        'pixdim_y': img_pix_size[1],
+                        'pixdim_z': img_pix_size[2],
 
-                         'resampled_pixdim_x': img_resampeld_pix_size[0],
-                         'resampled_pixdim_y': img_resampeld_pix_size[1],
-                         'resampled_pixdim_z': img_resampeld_pix_size[2],
+                        'resampled_pixdim_x': img_resampeld_pix_size[0],
+                        'resampled_pixdim_y': img_resampeld_pix_size[1],
+                        'resampled_pixdim_z': img_resampeld_pix_size[2],
 
-                         'size_x': img_size[0],
-                         'size_y': img_size[1],
-                         'size_z': img_size[2],
+                        'size_x': img_size[0],
+                        'size_y': img_size[1],
+                        'size_z': img_size[2],
 
-                         'resampled_size_x': img_resampeld_size[0],
-                         'resampled_size_y': img_resampeld_size[1],
-                         'resampled_size_z': img_resampeld_size[2],
+                        'resampled_size_x': img_resampeld_size[0],
+                        'resampled_size_y': img_resampeld_size[1],
+                        'resampled_size_z': img_resampeld_size[2],
 
-                         'filename': file,
-                         'image_path': file_path_main_images + '/' + file,
-                         'labe_path': file_path_main_labels + '/' + file}
+                        'filename': file,
+                        'image_path': file_path_main_images + '/' + file,
+                        'labe_path': file_path_main_labels + '/' + file,
 
-            df = df.append(dict_hold, ignore_index=True)
-            print()
-            print(idx)
+                        'train_val_test': train_val_test}
+
+            df_vol = df_vol.append(dict_vol, ignore_index=True)
+
+            img_data = img_resampled.get_fdata()
+            label_data = label_resampled.get_fdata()
+
+            for idx in range(img_data.shape[-1]):
+
+                img_slice = img_data[:, :, idx]
+                label_slice = label_data[:, :, idx]
 
 
-    df.to_csv('dataframe.csv', index=False)
+                img_slice_filename = 'prostate_' + str(file_split[-1]) + '_slice_' + str(idx) + '.npy'
+                label_slice_filename = 'label_' + str(file_split[-1]) + '_slice_' + str(idx) + '.npy'
 
-    pixdim_x = np.array(pixdim_x)
-    pixdim_y = np.array(pixdim_y)
-    pixdim_z = np.array(pixdim_z)
+                dict_slice = {'patient_number': file_split[-1],
+                              'slice_number': idx,
 
-    resampled_x_size = np.array(resampled_x_size)
-    resampled_y_size = np.array(resampled_y_size)
-    resampled_z_size = np.array(resampled_z_size)
+                              'resampled_pixdim_x': img_resampeld_pix_size[0],
+                              'resampled_pixdim_y': img_resampeld_pix_size[1],
+
+                              'resampled_size_x': img_resampeld_size[0],
+                              'resampled_size_y': img_resampeld_size[1],
+
+                              'image_filename': img_slice_filename,
+                              'label_filename': label_slice_filename,
+                              'train_val_test': train_val_test}
+
+
+
+                np.save('prostate_data' + '/' + img_slice_filename, img_slice)
+                np.save('prostate_data' + '/' + label_slice_filename, label_slice)
+
+                df_slice_sample = df_slice_sample.append(dict_slice, ignore_index=True)
+
+
+
+
+    df_vol.to_csv('dataframe_volume.csv', index=False)
+    df_slice_sample.to_csv('dataframe_slice.csv', index=False)
+
+    df_vol.to_pickle('dataframe_volume.pickle')
+    df_slice_sample.to_pickle('dataframe_slice.pickle')
+
