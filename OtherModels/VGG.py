f32a11fb4922edfb74893021989f21525b423869
gkuling <gkuling@gmail.com>
2019-03-18 11:43:21 -0400

    Updated the VGG network to have segmentation output

--- OtherModels/VGG.py
+++ OtherModels/VGG.py
@@ -5,26 +5,17 @@ Simple implementation of VGG as in 2014 paper (with no opportunity to significan
 '''
 
 from keras.layers import Input, MaxPooling2D, Conv2D, BatchNormalization, \
-    Activation, Deconvolution2D, UpSampling2D, concatenate, Dropout, Dense
+    Activation, Deconvolution2D, UpSampling2D, concatenate, Dropout, Dense, \
+    ZeroPadding2D, Reshape, Permute
 from keras.activations import softmax
 from keras.models import Model
 from keras import backend as K
 from keras import regularizers
 K.set_image_dim_ordering('tf')
 
-def get_vgg(img_x = 512,
-            img_y = 512,
-            optimizer = 'ADAM',
-            dilation_rate = 1,
-            kernel_initializer = 'glorot_uniform',
-            kernel_1d_size = 3,
-            depth = 5,
-            base_filter = 16,
-            batch_normalization = False,
-            pool_1d_size = 2,
-            deconvolution = False,
+def get_vgg(img_x = 256,
+            img_y = 256,
             dropout = 0,
-            num_classes = 3,
             num_seq = 1):
 
 
@@ -44,28 +35,33 @@ def get_vgg(img_x = 512,
     x = Conv2D(64, (3, 3), padding='same', activation='relu')(model_inputs)
     x = Conv2D(64, (3, 3), padding='same', activation='relu')(x)
     x = MaxPooling2D((2, 2), strides=2, padding='same')(x)
+    f1 = x
 
     x = Conv2D(128, (3, 3), padding='same', activation='relu')(x)
     x = Conv2D(128, (3, 3), padding='same', activation='relu')(x)
     x = MaxPooling2D((2, 2), strides=2, padding='same')(x)
+    f2 = x
 
     x = Conv2D(256, (3, 3), padding='same', activation='relu')(x)
     x = Conv2D(256, (3, 3), padding='same', activation='relu')(x)
     x = Conv2D(256, (3, 3), padding='same', activation='relu')(x)
     x = Conv2D(256, (3, 3), padding='same', activation='relu')(x)
     x = MaxPooling2D((2, 2), strides=2, padding='same')(x)
+    f3 = x
 
     x = Conv2D(512, (3, 3), padding='same', activation='relu')(x)
     x = Conv2D(512, (3, 3), padding='same', activation='relu')(x)
     x = Conv2D(512, (3, 3), padding='same', activation='relu')(x)
     x = Conv2D(512, (3, 3), padding='same', activation='relu')(x)
     x = MaxPooling2D((2, 2), strides=2, padding='same')(x)
+    f4 = x
 
     x = Conv2D(512, (3, 3), padding='same', activation='relu')(x)
     x = Conv2D(512, (3, 3), padding='same', activation='relu')(x)
     x = Conv2D(512, (3, 3), padding='same', activation='relu')(x)
     x = Conv2D(512, (3, 3), padding='same', activation='relu')(x)
     x = MaxPooling2D((2, 2), strides=2, padding='same')(x)
+    f5 = x
 
     x = Dense(4096, activation='relu', kernel_regularizer=regularizers.l2(5e-4))(x)
     x = Dropout(dropout)(x)
@@ -74,8 +70,32 @@ def get_vgg(img_x = 512,
     x = Dense(1000, activation='relu')(x)
     x = Activation(softmax)(x)
 
-    model = Model(model_inputs, x)
-    print(model.summary())
+    levels = [f1, f2, f3, f4, f5]
+
+    o = levels[2]
+
+    o = (ZeroPadding2D((1, 1)))(o)
+    o = (Conv2D(512, (3, 3), padding='valid'))(o)
+    o = (BatchNormalization())(o)
+
+    o = (UpSampling2D((2, 2)))(o)
+    o = (ZeroPadding2D((1, 1)))(o)
+    o = (Conv2D(256, (3, 3), padding='valid'))(o)
+    o = (BatchNormalization())(o)
+
+    o = (UpSampling2D((2, 2)))(o)
+    o = (ZeroPadding2D((1, 1)))(o)
+    o = (Conv2D(128, (3, 3), padding='valid'))(o)
+    o = (BatchNormalization())(o)
+
+    o = (UpSampling2D((2, 2)))(o)
+    o = (ZeroPadding2D((1, 1)))(o)
+    o = (Conv2D(64, (3, 3), padding='valid'))(o)
+    o = (BatchNormalization())(o)
+
+    o = Conv2D(1, (3, 3), padding='same')(o)
+    o = (Activation('softmax'))(o)
+    model = Model(model_inputs, o)
 
     return model
 
