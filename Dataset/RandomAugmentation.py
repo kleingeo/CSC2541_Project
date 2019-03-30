9e8641294cfc046bb59783f10f11ea48cdf5cf7a
BulwarkAssassin <inferiorbrain>
2019-03-30 17:47:13 -0400

    Added elastic transformation to RandomAugmentation

--- Dataset/RandomAugmentation.py
+++ Dataset/RandomAugmentation.py
@@ -2,7 +2,8 @@
 
 import numpy as np
 import scipy.ndimage as ndi
-
+from scipy.ndimage.interpolation import map_coordinates
+from scipy.ndimage.filters import gaussian_filter
 
 
 
@@ -110,6 +111,35 @@ def determine_operations(rescaling, translation, rotation, sheering,
             lr_flip_slice, ud_flip_slice)
 
 
+def __ElasticTransformation(image, alpha, sigma, random_state=None):
+    '''
+    Elastic deformation of images as described in [Simard2003]_.
+    .. [Simard2003] Simard, Steinkraus and Platt, "Best Practices for
+       Convolutional Neural Networks applied to Visual Document Analysis", in
+       Proc. of the International Conference on Document Analysis and
+       Recognition, 2003.
+    :param image: Input 2D image
+    :param alpha: Scaling factor. Controls intensity of the deformation.
+    :param sigma: Standard deviation of gaussian filter. Functionally the elasticity coefficient.
+    :return:
+    '''
+
+    assert len(image.shape) == 2
+
+    if random_state is None:
+        random_state = np.random.RandomState(None)
+
+    shape = image.shape
+
+    dx = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode="constant", cval=0) * alpha
+    dy = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode="constant", cval=0) * alpha
+
+    x, y = np.meshgrid(np.arange(shape[0]), np.arange(shape[1]), indexing='ij')
+    indices = np.reshape(x + dx, (-1, 1)), np.reshape(y + dy, (-1, 1))
+
+    return map_coordinates(image, indices, order=1).reshape(shape)
+
+
 def RandomAugmentation(t2_img, seg_img, t1_img=None, flair_img=None, sample_size=(256, 256),
                        scaling=None, translation=None, rotation=None, shearing=None,
                        lr_flip_slice=False, ud_flip_slice=False):
@@ -208,6 +238,9 @@ def RandomAugmentation(t2_img, seg_img, t1_img=None, flair_img=None, sample_size
 
         t2_data = ndi.interpolation.affine_transform(t2_data, transform_matrix, order=1)
         seg_data = ndi.interpolation.affine_transform(seg_data, transform_matrix, order=0)
+
+        t2_data = __ElasticTransformation(t2_data, alpha=720, sigma=24)
+        seg_data = __ElasticTransformation(seg_data, alpha=720, sigma=24)
 
         if t1_img is not None:
             t1_data = ndi.interpolation.affine_transform(t1_data, transform_matrix, order=1)
